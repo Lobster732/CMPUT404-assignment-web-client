7762c0e161b4ab63b749761b4e145b63377a545c
DylanCassidy <the.dylan.cassidy@gmail.com>
2015-02-02 09:44:13 -0700

    formated to PEP8 standards

--- httpclient.py
+++ httpclient.py
@@ -115,29 +115,29 @@ class HTTPClient(object):
         return HTTPRequest(code, body)
 
     def command(self, url, command="GET", args=None):
-        print ""  # TODO: Remove
-        print "In function 'command'"  # TODO: Remove
-        print "url = ", url, "| command = ", command, \
-            "| args = ", args  # TODO: Remove
+        #print ""  # TODO: Remove
+        #print "In function 'command'"  # TODO: Remove
+        #print "url = ", url, "| command = ", command, \
+            #"| args = ", args  # TODO: Remove
         if (command == "POST"):
             return self.POST(url, args)
         else:
             return self.GET(url, args)
 
 if __name__ == "__main__":
-    print ""  # TODO: Remove
-    print "In function 'main'"  # TODO: Remove
+    #print ""  # TODO: Remove
+    #print "In function 'main'"  # TODO: Remove
     client = HTTPClient()
     command = "GET"
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print "argv[1] = ", sys.argv[1], "| argv[2] = ", \
-            sys.argv[2]  # TODO: Remove
+        #print "argv[1] = ", sys.argv[1], "| argv[2] = ", \
+            #sys.argv[2]  # TODO: Remove
         # print client.command( sys.argv[1], sys.argv[2] )  # TODO: Fix Maybe
         print client.command(sys.argv[2], sys.argv[1])
     else:
-        print "command", command, "| argv[1] = ", sys.argv[1]  # TODO: Remove
+        #print "command", command, "| argv[1] = ", sys.argv[1]  # TODO: Remove
         # print client.command( command, sys.argv[1] )  # TODO: Fix Maybe
         print client.command(sys.argv[1], command)
